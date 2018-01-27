d478d2ef8b2a27a6e3c6694774cf0f0d31738af5
carlosm <carlosjmr12@gmail.com>
2018-01-27 00:57:47 -0400

    #falta añadir iconos izquierdos, borrar derechos matarnos con el mapa al final pasar los datos jsonear con volley(opcional por ahora) borralo.
    el patrangana, el pana de martin elias

---
+++ app/src/main/java/com/example/marci/localizacionsola/perfil.java
@@ -0,0 +1,42 @@
+package com.example.marci.localizacionsola;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ImageViewTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+
+public class perfil extends AppCompatActivity {
+String img;
+ImageView ivperfil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ivperfil= (ImageView) findViewById(R.id.IVperfil);
+       /* Glide.with(getApplicationContext()).load(img).into(ivperfil);*/
+    }
+}
